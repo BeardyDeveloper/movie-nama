b719fab52afaf086c805b499a6305e53ec2132ea
Mohammad Mirzaei <beardydeveloper@gmail.com>
2023-04-29 00:06:37 +0430

    feat: config inline svg imports for nextjs

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
 module.exports = {
   presets: ['next/babel'],
   plugins: [
+    [
+      'inline-react-svg',
+      {
+        svgo: {
+          plugins: [
+            {
+              name: 'preset-default',
+              params: {
+                overrides: {
+                  cleanupIDs: false,
+                },
+              },
+            },
+          ],
+        },
+      },
+    ],
     ['styled-components', { ssr: true, displayName: true, preprocess: false }],
   ],
 };
