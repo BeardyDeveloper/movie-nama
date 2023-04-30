cb0550af4797b7b75578cd312ac7769a5da1f96f
Mohammad Mirzaei <beardydeveloper@gmail.com>
2023-04-30 16:09:50 +0430

    fix: add prop types to selectStyles file of select component

--- src/components/shared/SelectInput/selectStyles.ts
+++ src/components/shared/SelectInput/selectStyles.ts
@@ -1,6 +1,10 @@
-import { ContainerProps, OptionProps, SingleValueProps } from 'react-select';
+import {
+  CSSObjectWithLabel,
+  ContainerProps,
+  OptionProps,
+  SingleValueProps,
+} from 'react-select';
 import { ITheme } from '@theme/ITheme';
-import colorAlpha from 'color-alpha';
 
 interface SelectStylesProps {
   menuWidth?: string;
@@ -15,7 +19,7 @@ export const selectStyles = ({
   theme,
   error,
 }: SelectStylesProps) => ({
-  container: (styles: any, state: ContainerProps) => {
+  container: (styles: CSSObjectWithLabel, state: ContainerProps) => {
     return {
       ...styles,
       'height': 'auto',
@@ -45,7 +49,7 @@ export const selectStyles = ({
       },
     };
   },
-  menu: (styles: any) => ({
+  menu: (styles: CSSObjectWithLabel) => ({
     ...styles,
     width: menuWidth ?? '100%',
     left: menuLeft ?? 0,
@@ -56,12 +60,13 @@ export const selectStyles = ({
     padding: '8px 0',
     height: 'auto',
   }),
-  menuList: (styles: any) => ({
+  menuList: (styles: CSSObjectWithLabel) => ({
     ...styles,
     height: '325px',
     maxHeight: '325px',
   }),
-  menuPortal: (styles: any) => ({ ...styles, zIndex: 9999 }),
+  menuPortal: (styles: CSSObjectWithLabel) => ({ ...styles, zIndex: 9999 }),
+  // incompatible styles types!
   option: (styles: any, state: OptionProps) => {
     return {
       ...styles,
@@ -90,7 +95,7 @@ export const selectStyles = ({
       },
     };
   },
-  input: (styles: any) => ({
+  input: (styles: CSSObjectWithLabel) => ({
     ...styles,
     fontFamily: 'Gilroy-SemiBold',
     borderRadius: '8px',
@@ -99,18 +104,18 @@ export const selectStyles = ({
     caretColor: theme.cmp.input.placeholder,
     margin: 0,
   }),
-  valueContainer: (styles: any) => ({
+  valueContainer: (styles: CSSObjectWithLabel) => ({
     ...styles,
     padding: '2px 8px',
     margin: 0,
   }),
-  placeholder: (styles: any) => ({
+  placeholder: (styles: CSSObjectWithLabel) => ({
     ...styles,
     color: theme.cmp.input.placeholder,
     fontSize: theme.typography.fontSize.body.M,
     margin: 0,
   }),
-  control: (styles: any) => ({
+  control: (styles: CSSObjectWithLabel) => ({
     ...styles,
     'width': '100%',
     'minHeight': '44px',
@@ -130,7 +135,7 @@ export const selectStyles = ({
     'justifyContent': 'space-between',
     'alignitems': 'center',
   }),
-  dropdownIndicator: (styles: any) => ({
+  dropdownIndicator: (styles: CSSObjectWithLabel) => ({
     ...styles,
     'color': theme.palette.Neutral[400],
     '&:hover': {
@@ -138,7 +143,7 @@ export const selectStyles = ({
     },
     'cursor': 'pointer',
   }),
-  clearIndicator: (styles: any) => ({
+  clearIndicator: (styles: CSSObjectWithLabel) => ({
     ...styles,
     'cursor': 'pointer',
     'color': theme.palette.Neutral[400],
@@ -149,24 +154,24 @@ export const selectStyles = ({
   indicatorSeparator: () => ({
     display: 'none',
   }),
-  singleValue: (styles: any, state: SingleValueProps) => ({
+  singleValue: (styles: CSSObjectWithLabel, state: SingleValueProps) => ({
     ...styles,
     opacity: state.isDisabled ? 0.5 : 1,
     transition: 'opacity 300ms',
     color: theme.cmp.input.color,
     margin: 0,
   }),
-  multiValue: (styles: any) => {
+  multiValue: (styles: CSSObjectWithLabel) => {
     return {
       ...styles,
       backgroundColor: theme.palette.Neutral[700],
     };
   },
-  multiValueLabel: (styles: any) => ({
+  multiValueLabel: (styles: CSSObjectWithLabel) => ({
     ...styles,
     color: theme.cmp.input.color,
   }),
-  multiValueRemove: (styles: any) => ({
+  multiValueRemove: (styles: CSSObjectWithLabel) => ({
     ...styles,
     cursor: 'pointer',
     color: theme.palette.Status.Rose[400],
